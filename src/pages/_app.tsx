8c8add19cdf567741fbf99f73c642036e3db3056
Jay Stothard <jtstothard@gmail.com>
2023-03-18 18:31:39 +0000

    add combobox

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,29 @@
 import { type AppType } from "next/app";
 
-import { api } from "~/lib/api";
+import { api } from "@/lib/api";
 
 import "@total-typescript/ts-reset";
-import "~/styles/globals.css";
+import "@/styles/globals.css";
 
-import { Inter as FontSans } from "next/font/google"
+import { Inter as FontSans } from "next/font/google";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
   display: "swap",
-})
+});
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <main className={fontSans.className}>
-    <Component {...pageProps} />;
-  </main>
+  return (
+    <>
+      <style jsx global>{`
+    :root {
+      --font-sans: ${fontSans.style.fontFamily};
+    }
+  }`}</style>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 
-import { api } from "~/lib/api";
+import { api } from "@/lib/api";
+import { CommandCombobox } from "@/components/combobox";
 
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
@@ -11,16 +12,31 @@ const Home: NextPage = () => {
       <Head>
         <title>Politoed</title>
         <meta name="description" content="Pokemon VGC team builder" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-        <link rel="manifest" href="/site.webmanifest"/>
-        <link rel="mask-icon" href="/safari-pinned-ta/b.svg" color="#5bbad5"/>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-ta/b.svg" color="#5bbad5" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center">
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-          Politoed 
+          Politoed
         </h1>
+        <CommandCombobox />
         <p className="text-xl text-slate-700 dark:text-slate-400">
           {hello?.data?.greeting}
         </p>
